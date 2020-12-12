53efc15d7e210f2d3393c30d7492cf88eb2f254a
Tâm Trần Thanh <18127268@student.hcmus.edu.vn>
2020-12-12 13:16:51 +0700

    home price

--- tam_minh_12_08/controllers/home.route.js
+++ tam_minh_12_08/controllers/home.route.js
@@ -4,13 +4,32 @@ const productModel = require('../models/product.model');
 const router = express.Router();
 
 router.get('/', async function (req, res, next) {
-    const newest = await productModel.topTenNewest();
-    const viewed = await productModel.topTenViewed();
-    const rating = await productModel.topFiveRating();
+    var newest = await productModel.topTenNewest();
+    var viewed = await productModel.topTenViewed();
+    var rating = await productModel.topFiveRating();
+
+    console.log(typeof (newest));
+
+    for (let i = 0; i < newest.length; i++) {
+        var newprice = (newest[i].price * (100 - newest[i].discount) / 100);
+        newest[i]["newprice"] = newprice;
+    }
+
+    for (let i = 0; i < rating.length; i++) {
+        var newprice = (rating[i].price * (100 - rating[i].discount) / 100);
+        rating[i]["newprice"] = newprice;
+    }
+
+    for (let i = 0; i < viewed.length; i++) {
+        var newprice = (viewed[i].price * (100 - viewed[i].discount) / 100);
+        viewed[i]["newprice"] = newprice;
+    }
+
+    console.log(newest);
     res.render('home', {
         newest: newest,
         viewed: viewed,
-        rating: rating
+        rating: rating,
     });
     // try {
     //   const list = await productModel.all();
--- tam_minh_12_08/models/product.model.js
+++ tam_minh_12_08/models/product.model.js
@@ -23,19 +23,19 @@ module.exports = {
   },
 
   async topTenNewest() {
-    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by publish_day desc limit 10;';
+    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co left  join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by publish_day desc limit 10;';
     const [rows, fields] = await db.load(sql);
     return rows;
   },
 
   async topTenViewed() {
-    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by view_number desc limit 10;';
+    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co left  join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by view_number desc limit 10;';
     const [rows, fields] = await db.load(sql);
     return rows;
   },
 
   async topFiveRating() {
-    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by rating desc limit 5;';
+    const sql = 'select *, DATEDIFF(curdate(), co.publish_day) as day_ago from course co left join (select course_id, avg(rating) as rating, count(student_id) as numberStu from stu_registerlist group by course_id) ra on co.course_id = ra.course_id join course_spec cs on co.spec = cs.spec_id join teacher te on co.teacher_id = te.teacher_id join course_type ct on co.type=ct.type_id order by rating desc limit 5;';
     const [rows, fields] = await db.load(sql);
     return rows;
   },
