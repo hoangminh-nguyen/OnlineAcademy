23ca426afbbdd453a65f69e872ea900abf3f0f93
trongtaiz <51943367+trongtaiz@users.noreply.github.com>
2020-12-17 15:25:28 +0700

    all course

--- tam_minh_12_08/controllers/courses.route.js
+++ tam_minh_12_08/controllers/courses.route.js
@@ -25,4 +25,19 @@ router.get('/detail/:id', async function (req, res, next) {
     });
 })
 
+router.get('/all', async (req, res) => {
+    // var register = await courseModel.all();
+    // var register = await courseModel.topFiveRegisterInSpec(course["spec"]);
+    // for (let i = 0; i < register.length; i++) {
+    //     var newprice = (register[i].price * (100 - register[i].discount) / 100);
+    //     register[i]["newprice"] = newprice;
+    // }
+
+    // var newprice = (course.price * (100 - course.discount) / 100);
+    // course["newprice"] = newprice;
+    res.render('vwCourses/all',{ 
+
+    })
+})
+
 module.exports = router;
