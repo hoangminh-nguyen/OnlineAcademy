e0a30335c485e63a35b5044e8e042c1e335e02e7
Minh Nguyễn <minh.nguyen28122000@gmail.com>
2021-01-04 01:54:37 +0700

    add product to watchlist and modify menu roll

--- tam_minh_12_08/models/student.model.js
+++ tam_minh_12_08/models/student.model.js
@@ -23,6 +23,18 @@ module.exports = {
         const [result, fields] = await db.del(condition, 'student');
         return result;
     },
+
+    async addWatchlistItem(courseid, studentid) {
+        const sql = `insert into stu_watchlist  values(${studentid},${courseid})`;
+        const [rows, fields] = await db.load(sql);
+    },
+    async findWatchlistItem(courseid, studentid) {
+        const sql = `select * from stu_watchlist st where st.student_id=${studentid} and st.course_id =${courseid}`;
+        const [rows, fields] = await db.load(sql);
+        if (rows.length === 0)
+            return null;
+        return rows;
+    },
     async delWatchlistItem(courseid, studentid) {
         const sql = `delete from stu_watchlist where student_id=${studentid} and course_id =${courseid}`;
         const [rows, fields] = await db.load(sql);
